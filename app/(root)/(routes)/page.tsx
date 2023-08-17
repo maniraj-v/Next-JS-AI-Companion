f781cd6d516e8fe5fcf0b441efdb23a93eabcb4d
Mani Velrajan <mani.velrajan.ctr@ihstowers.com>
2023-08-17 23:01:07 +0530

    Companions card component Homepage

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,14 +2,40 @@ import { UserButton } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
 import SearchInput from "@/components/search-input";
 import Categories from "@/components/categories";
+import Companions from "@/components/companions";
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+export default async function Home({ searchParams }: HomeProps) {
   const categories = await prismadb.category.findMany();
-
+  const companions = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+  
   return (
     <div className="p-4 h-full space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={companions} />
     </div>
   );
 }
